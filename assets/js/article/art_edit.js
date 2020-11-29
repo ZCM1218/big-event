7b2147897c02c91f619533798850de33a42e9d17
ZCM1218 <839037420@qq.com>
2020-11-29 14:25:14 +0800

    项目完成

--- assets/js/article/art_edit.js
+++ assets/js/article/art_edit.js
@@ -2,26 +2,52 @@ $(function () {
   let layer = layui.layer;
   let state = '';
   let form = layui.form;
+  // 截取url中的id
+  let id = location.search.split('?id=')[1];
   function renderForm() {
     layui.use('form', function () {
       form.render();
     });
   };
+  // 获取文章类别
   $.ajax({
     url: '/my/article/cates',
     success: function (res) {
       if (res.status !== 0) {
         return layer.msg('获取文章分类列表失败！');
       }
-      console.log(res);
+      // console.log(res);
       let htmlStr = template('artCateTpl', res);
       $('select').append(htmlStr);
       renderForm();
+      // 获取到文章类别信息之后要再给表单进行赋值操作
+      getArgInfo();
     }
   })
+  // 根据id获取文章详情
+  function getArgInfo() {
+    $.ajax({
+      url: '/my/article/' + id,
+      success: function (res) {
+        console.log(res);
+        if (res.status !== 0) {
+          return layer.msg('获取文章信息失败！');
+        }
+        // 初始化富文本编辑器
+        initEditor();
+        // 修改文章的发布状态
+        state = res.data.state;
+        form.val('form', res.data);
+        // 3. 初始化裁剪区域
+        // 通过后台响应的文章详情来更新封面裁剪区域
+        // res.data.cover_img ==> 得到的图片没有根路径
+        $image
+          .attr("src", "http://ajax.frontend.itheima.net" + res.data.cover_img)
+          .cropper(options);
+      }
+    })
+  }
 
-  // 初始化富文本编辑器
-  initEditor();
 
   // 1. 初始化图片裁剪器
   let $image = $('#image')
@@ -32,8 +58,10 @@ $(function () {
     preview: '.img-preview'
   }
 
+  // 编辑界面不需要这个，因为裁剪区域的图片地址是通过请求该文章的数据来获得的，如果加了这个，就又恢复默认的了
+  // 在上面，获取到文章详情之后，通过res.data.cover_img来初始化裁剪区域
   // 3. 初始化裁剪区域
-  $image.cropper(options);
+  // $image.cropper(options);
 
   // 当文件域表单发生改变的时候的事件处理程序
   $('.chooseCover').on('click', function () {
@@ -73,14 +101,17 @@ $(function () {
         let fd = new FormData(this);
         fd.append("state", state);
         fd.append("cover_img", blob);
+        // 添加文章Id
+        fd.append('Id', id);
         fd.forEach(item => console.log(item));
         publish(fd);
       })
   })
 
+  // 更新文章，发送请求，fd带上文章Id（一开始跟在url后面的）
   function publish(fd) {
     $.ajax({
-      url: '/my/article/add',
+      url: '/my/article/edit',
       type: 'POST',
       data: fd,
       contentType: false,
@@ -88,9 +119,9 @@ $(function () {
       success: function (res) {
         console.log(res);
         if (res.status !== 0) {
-          return layer.msg('发表文章失败！');
+          return layer.msg('修改文章失败！');
         }
-        layer.msg('文章发布成功，即将跳转至文章列表界面！',
+        layer.msg('文章修改成功，即将跳转至文章列表界面！',
           { time: 2000 },
           function () {
             location.href = '/article/art_list.html'
@@ -98,4 +129,9 @@ $(function () {
       }
     })
   }
+
+  // 以上内容都是跟发表文章一样的，编辑文章需要的就是根据id获取到此文章的信息，填充到表单中去
+
 })
+
+// 封装函数就会出现富文本编辑器没内容的bug，不封装就会出现文章类别没内容和封面没法显示的bug
--- assets/js/article/art_list.js
+++ assets/js/article/art_list.js
@@ -1,45 +1,90 @@
 $(function () {
   let layer = layui.layer;
+  let form = layui.form;
+  let laypage = layui.laypage;
   let query = {
     pagenum: 1,
-    pagesize: 2,
+    pagesize: 3,
     cate_id: '',
     state: ''
   }
-  // layui表单的重新渲染
-  function renderForm() {//在这里把重新渲染的功能包装秤一个方法
-    layui.use('form', function () {
-      var form = layui.form;
-      form.render();
-    });
-  }
-
+  // 获取文章类别列表，如果通过 文章列表 获取文章类别，会有重复的
   $.ajax({
-    url: '/my/article/list',
-    data: query,
+    url: '/my/article/cates',
     success: function (res) {
-      console.log(res);
       if (res.status !== 0) {
         return layer.msg('获取文章信息失败！');
       }
-      let htmlStr1 = template('artListTpl', res);
-      $('tbody').append(htmlStr1);
-
-      let htmlStr2 = template('allCateTpl', res);
-      // console.log(htmlStr2);
-      $('#allCate').append(htmlStr2);
-      let htmlStr3 = template('allStateTpl', res);
-      // console.log(htmlStr3);
-      $('#allState').append(htmlStr3);
-      renderForm();
+      res.data.forEach(function (item) {
+        let htmlStr1 = `<option value="${item.Id}">${item.name}</option>`;
+        $('#allCate').append(htmlStr1);
+      })
+      form.render();
     }
   })
 
-  $('#allCate').on('click', function () {
-    console.log('hh');
+  // 根据分类和状态筛选
+  $('.layui-form').on('submit', function (e) {
+    e.preventDefault();
+    // console.log('hh');
+    query.cate_id = $('[name = cate_id]').val();
+    query.state = $('[name = state]').val();
+    getArtList();
   })
 
+
+  // 获取文章列表信息
+  getArtList();
+  function getArtList() {
+    $.ajax({
+      url: '/my/article/list',
+      data: query,
+      success: function (res) {
+        console.log(res);
+        if (res.status !== 0) {
+          layer.msg('获取文章列表信息失败！');
+        }
+        let htmlStr = template('artListTpl', res);
+        $('tbody').empty();
+        $('tbody').append(htmlStr);
+        renderPage(res.total);
+      }
+    })
+  }
+
+  // 分页
+  function renderPage(total) {
+    //执行一个laypage实例
+    laypage.render({
+      curr: query.pagenum,
+      limit: query.pagesize,
+      elem: 'artList', //注意，这里的 test1 是 ID，不用加 # 号
+      count: total, //数据总数，从服务端得到
+      limits: [1, 2, 3, 4, 5],
+      layout: ["count", "limit", "prev", "page", "next", "skip"],
+      // 回调函数
+      jump: function (obj, first) {
+        //obj包含了当前分页的所有参数，比如：
+        console.log(obj.curr);
+        //得到当前页，以便向服务端请求对应页的数据。
+        query.pagenum = obj.curr;
+        console.log(obj.limit);
+        //得到每页显示的条数
+        query.pagesize = obj.limit;
+        // 先配置数据，再进行渲染
+        // 注意：只有在点击了其他页之后才进行渲染，否则会重复发送请求进行渲染
+        if (!first) {
+          // 点击的时候会让if成立
+          // console.log("点击了");
+          getArtList();
+        }
+      }
+    });
+  }
+
+  // 点击删除按钮获取Id，根据Id来发送删除文章的请求
   $('body').on('click', '.delBtn', function () {
+    console.log($(this).attr('data-id'));
     $.ajax({
       url: '/my/article/delete/' + $(this).attr('data-id'),
       success: function (res) {
@@ -49,10 +94,24 @@ $(function () {
         }
         layer.confirm('确认删除文章？', { icon: 3, title: '删除文章' }, function (index) {
           //do something
-
+          // 判断，如果最后一页只有一篇文章，删除之后自动跳到前一页，否则会停留在没有数据的最后一页
+          if (($('.delBtn')).length === 1) {
+            query.pagenum = query.pagenum - 1;
+          }
+          getArtList();
           layer.close(index);
         });
       }
     })
   })
+  // 编辑文章
+  $('body').on('click', '.editBtn', function () {
+    // console.log('hh');
+    // 在这个页面发送的请求获取到的数据不能在其他页面使用，需要跳转页面之后，再根据id来获取文章详情
+    // 因为跳转到编辑界面之后，不能获取到此文章的id了，所以要将id保存起来，对于比较少的数据可以拼接在url后面了
+    location.href = '/article/art_edit.html?id=' + $(this).attr('data-id');
+    // console.log($(this).attr('data-id'));
+  })
+
+
 })
